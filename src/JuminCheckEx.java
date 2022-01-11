3db0a78d5afc72f68e8533b70b0510f3fd869eb1
msdm3328@naver.com <msdm3328@naver.com>
2022-01-11 20:06:24 +0900

    0111code

---
+++ src/JuminCheckEx.java
@@ -0,0 +1,18 @@
+
+public class JuminCheckEx {
+	public static void main(String[] args) {
+		//준비 단계 
+		String juminNum;
+		int sum = 0;	
+		int[] 
+		//입력 단계
+		
+			
+		//처리 단계
+		
+		//출력 단계
+		
+
+	}
+
+}
